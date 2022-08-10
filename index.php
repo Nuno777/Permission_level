51cb8c79410be25465c6ac55e67d6b0e7dfbc5ea
Nuno Miguel <nm.albano20@gmail.com>
2022-08-10 04:06:56 +0100

    design dashboard

--- index.php
+++ index.php
@@ -47,10 +47,10 @@
           <div class="row align-items-center">
 
             <div class="col-6 col-xl-2">
-              <h1 class="mb-0 site-logo"><a href="index.html" class="h2 mb-0">Bank<span
+              <h1 class="mb-0 site-logo"><a href="index.php" class="h2 mb-0">Bank<span
                     class="text-primary">.</span> </a></h1>
             </div>
-
+  
             <div class="col-12 col-md-10 d-none d-xl-block">
               <nav class="site-navigation position-relative text-right"
                 role="navigation">
